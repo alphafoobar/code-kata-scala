95f971fd921883181f2bbb79328fa2080adbe225
James <alphafoobar@users.noreply.github.com>
2019-05-02 11:37:04 +1200

    Apply algorithm from code-kata-erlang

--- src/main/scala/kata02/BinarySearch.scala
+++ src/main/scala/kata02/BinarySearch.scala
@@ -8,17 +8,16 @@ class BinarySearch {
 
         val mid = array.length / 2
         val item = array(mid)
-
-            if (item > key) {
-                val x = chop(key, array.slice(0, mid - 1))
-                return x
-            }
-
-            if (item < key) {
-                val x = chop(key, array.slice(mid + 1, array.length))
-                return if (x < 0) x else mid + x
-            }
-
-        return if (item == key) mid else -1
+        if (item == key) {
+            return mid;   
+        }
+        if (array.length > 1 && item > key) {
+            return chop(key, array.slice(0, mid - 1))
+        }
+        if (array.length > 1 && item < key) {
+            val x = chop(key, array.slice(mid + 1, array.length))
+            return if (x == -1) x else mid + x
+        }
+        return -1
     }
 }
